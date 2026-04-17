6a9c820d576b49f66872257815443c98912ac07a
Mario Metshein <metshein@gmail.com>
2026-04-17 11:43:39 +0300

    feat: implement car details update functionality with form handling

--- muuda.php
+++ muuda.php
@@ -1,22 +1,31 @@
 <?php
     include("config.php");
 
-    // if (!empty($_GET)) {
-    //     $mark = $_GET['mark'];
-    //     $model = $_GET['model'];
-    //     $engine = $_GET['engine'];
-    //     $fuel = $_GET['fuel'];
-    //     $price = $_GET['price'];
-    //     $image = $_GET['image'];
+    if (!empty($_GET['muuda_id'])) {
+         $id = $_GET['muuda_id'];
+        $mark = $_GET['mark'];
+        $model = $_GET['model'];
+        $engine = $_GET['engine'];
+        $fuel = $_GET['fuel'];
+        $price = $_GET['price'];
+        $image = $_GET['image'];
+        
+        $paring = "UPDATE cars 
+        SET mark = '".$mark."', 
+        model = '".$model."', 
+        engine = '".$engine."', 
+        fuel = '".$fuel."', 
+        price = '".$price."', 
+        image = '".$image."' 
+        WHERE cars.id = ".$id."";
 
-        // $paring = "INSERT INTO cars (mark, model, engine, fuel, price, image) 
-        // VALUES ( '".$mark."', '".$model."', '".$engine."', '".$fuel."', '".$price."', '".$image."')";
-       // $tulemus = mysqli_affected_rows($yhendus);
-        // if ($tulemus == 1) {
-        //     header("Location: admin.php");
-        // }
-        // }
-
+        print($paring);
+        $valjund = mysqli_query($yhendus, $paring);
+        $tulemus = mysqli_affected_rows($yhendus);
+            if ($tulemus == 1) {
+                header("Location: admin.php");
+            }
+        }
 
         $paring = "SELECT * FROM cars WHERE id=".$_GET['id']."";
         $valjund = mysqli_query($yhendus, $paring);
@@ -25,6 +34,7 @@
 ?>
        
 <form action="muuda.php" method="get">
+    <input type="hidden" name="muuda_id" value="<?php echo $rida['id']; ?>"><br>
     Mark <input type="text" name="mark" value="<?php echo $rida['mark']; ?>"><br>
     Model <input type="text" name="model" value="<?php echo $rida['model']; ?>"><br>
     Engine <input type="text" name="engine" value="<?php echo $rida['engine']; ?>"><br>
